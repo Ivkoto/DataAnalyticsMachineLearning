d9ee86ed7dacb4e8f5a97bef8e55f546e3943353
Ivaylo Kostov <Ivaylo.Kostov@devocean.solutions>
2023-10-09 16:49:02 +0300

    ML Notes updated. Added cleasing data jupiter nb. Links notes updated.

--- Notes/ML_Notes.py
+++ Notes/ML_Notes.py
@@ -22,3 +22,81 @@
   # Large datasets are usually characterized by their size, which can range from hundreds of thousands to millions or even billions 
   # of data points. Handling and training models on large datasets can be computationally intensive and may require distributed 
   # computing resources or specialized hardware. Large datasets are common in fields like deep learning and big data analytics.
+
+
+#     Supervised learning: Classification, Regression, Decision Trees
+
+
+
+#                              Regression:
+#
+#     Linear Regression:
+#         Objective:  aims to find a linear relationship between the independent variables and the dependent variable. 
+#                     It minimizes the sum of squared differences between the actual and predicted values.
+#         
+#         Regularization: Linear Regression does not include regularization terms, making it prone to overfitting when dealing with 
+#                         high-dimensional datasets or datasets with multicollinearity.
+#         
+#         Use Cases:  Linear Regression is suitable for datasets where the relationship between variables is approximately linear, 
+#                     there are few features, and overfitting is not a significant concern.
+# 
+#     Ridge Regression:
+#         Objective:  extension of Linear Regression that adds L2 regularization. It minimizes the sum of squared differences 
+#                     while penalizing large coefficients by adding a regularization term.
+#         
+#         Regularization: Ridge Regression introduces L2 regularization, which helps prevent overfitting by reducing the magnitude of coefficients. 
+#                         It can handle multicollinearity better than Linear Regression.
+#         
+#         Use Cases:  Ridge Regression is useful when dealing with datasets that have multicollinearity (highly correlated features) 
+#                     and when you want to prevent overfitting. It is suitable for a wide range of datasets, including those with many features.
+# 
+#     Lasso Regression:
+#         Objective:   another extension of Linear Regression that adds L1 regularization. 
+#                     Like Ridge, it minimizes the sum of squared differences but with an added penalty term.
+#         
+#         Regularization: Lasso Regression introduces L1 regularization, which not only prevents overfitting but also performs feature selection 
+#                         by driving some feature coefficients to exactly zero. It can help simplify models by removing irrelevant features.
+#         
+#         Use Cases:  Lasso Regression is particularly useful when dealing with high-dimensional datasets where feature selection is important.
+#                     It can automatically identify and select the most relevant features while providing a sparse model.
+# 
+
+#     Summary: 
+#       * Linear Regression is a basic model without regularization and is suitable for simple, low-dimensional datasets.
+#       * Ridge Regression is suitable for datasets with multicollinearity and helps prevent overfitting by controlling coefficient magnitudes.
+#       * Lasso Regression is suitable for high-dimensional datasets and provides both feature selection and regularization.
+# 
+#     The choice between these regression techniques depends on the nature of your dataset and your specific modeling goals. 
+#       * Ridge and Lasso are often preferred when dealing with more complex and high-dimensional data.
+#       * Linear Regression may suffice for simpler, low-dimensional problems. 
+#       * Cross-validation and hyperparameter tuning can help determine the most suitable regression approach for your specific dataset.
+
+
+
+#                        Decision Tree:
+
+#         Objective: used for both classification and regression. 
+#                    They aim to create a tree-like structure of decisions based on input features that leads to predictions or decisions 
+#                    regarding the target variable.
+#
+#         Regularization:  can be prone to overfitting, especially when they grow deep trees. 
+#                          To control overfitting, you can use techniques like tree pruning or limiting the maximum depth of the tree.
+#
+#         Use Cases:
+#             Classification: suitable for datasets with both categorical and numerical features. They work well when the decision boundaries
+#                             between classes are non-linear or involve complex interactions between features.
+#             Regression: used for regression tasks, but they may not perform as well as linear models when the relationship between features 
+#                         and the target variable is primarily linear.
+#        
+#        
+#                        Random Forest:
+#        
+#         Objective: ensemble learning method based on Decision Trees. It builds multiple Decision Trees and combines their predictions 
+#                    to improve accuracy and reduce overfitting.
+#        
+#         Regularization: inherently reduces overfitting by aggregating the predictions of multiple trees. It also provides a feature 
+#                         importance score that can help identify the most relevant features.
+#        
+#         Use Cases: Random Forest is versatile and can be used for a wide range of datasets, including high-dimensional datasets. 
+#                    It's especially effective when dealing with datasets with noisy or irrelevant features.
+
--- Useful_Links.md
+++ Useful_Links.md
@@ -1,8 +1,8 @@
 **An Introduction to Statistical Learning:** \
 https://www.statlearning.com/?fbclid=IwAR0cO_xcxHLugqyJ5VCdhaEybFAxTV6gQP9Tlnpwv94X09WlZWG2BTJRRvA
 
-?\
+**?\**
 https://maps.pobal.ie/WebApps/DeprivationIndices/index.html?fbclid=IwAR1VsBhFKwP8WjPPXEBDI60qk2JhaHEIIBszYYzfsxM7lWFUjE1IRPNlj6o
 
-Annual Population Estimates:
+**Annual Population Estimates:**
 https://data.cso.ie/
